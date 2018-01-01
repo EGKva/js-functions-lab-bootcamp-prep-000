12de84bd65eb10f561d069e010b5dd28e81dc192
Ed Keithly <edward.keithly@gmail.com>
2018-01-01 23:54:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function happyHolidays() {
 function happyHolidaysTo(firstName) {
   return `Happy holidays, ${firstName}!`
 }
+
+function happyHolidayTo(holiday, firstName) {
+  return `Happy ${holiday}, ${firstName}!`
+}
