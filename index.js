9c9f4595583b52afb0b17cae376c54d6efd424bc
Ed Keithly <edward.keithly@gmail.com>
2018-01-01 23:47:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+
+function happyHolidays() {
+  var greeting = 'Happy holidays!'
+  return greeting
+  }
+function happHolidayTo(string) {
+  }
+function holidayCountdown {
+  }
